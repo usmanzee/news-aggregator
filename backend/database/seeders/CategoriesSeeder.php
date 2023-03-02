47a9263becfe976069a09aedcebccdfae7db6d21
usmanzee <usman.jamil0308@gmail.com>
2023-03-02 20:44:03 +0500

    gitignore cache removed

---
+++ backend/database/seeders/CategoriesSeeder.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class CategoriesSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        if(DB::table('categories')->count() == 0){
+            DB::table('categories')->insert([
+                [
+                    'value' => 'general',
+                    'title' => 'General',
+                    'created_at' => date('Y-m-d H:i:s'),
+                    'updated_at' => date('Y-m-d H:i:s'),
+                ],
+                [
+                    'value' => 'business',
+                    'title' => 'Business',
+                    'created_at' => date('Y-m-d H:i:s'),
+                    'updated_at' => date('Y-m-d H:i:s'),
+                ],
+                [
+                    'value' => 'entertainment',
+                    'title' => 'Entertainment',
+                    'created_at' => date('Y-m-d H:i:s'),
+                    'updated_at' => date('Y-m-d H:i:s'),
+                ],
+                [
+                    'value' => 'health',
+                    'title' => 'Health',
+                    'created_at' => date('Y-m-d H:i:s'),
+                    'updated_at' => date('Y-m-d H:i:s'),
+                ],
+                [
+                    'value' => 'science',
+                    'title' => 'Science',
+                    'created_at' => date('Y-m-d H:i:s'),
+                    'updated_at' => date('Y-m-d H:i:s'),
+                ],
+                [
+                    'value' => 'sports',
+                    'title' => 'Sports',
+                    'created_at' => date('Y-m-d H:i:s'),
+                    'updated_at' => date('Y-m-d H:i:s'),
+                ],
+                [
+                    'value' => 'technology',
+                    'title' => 'Technology',
+                    'created_at' => date('Y-m-d H:i:s'),
+                    'updated_at' => date('Y-m-d H:i:s'),
+                ],
+            ]);
+            
+        } else { echo "\e[31mTable is not empty, therefore NOT "; }
+    }
+}
